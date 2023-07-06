2e2fceb0408272b8679eedeac1467b959354a99e
Ibnu Maksum <ibnumaksum@gmail.com>
2023-07-06 18:02:51 +0700

    fix wrong file

--- bkash.php
+++ bkash.php
@@ -1,187 +1,229 @@
-
 <?php
-
 
 /**
  * PHP Mikrotik Billing (https://github.com/hotspotbilling/phpnuxbill/)
  *
- * Payment Gateway paypal.com
- *
- * created by @ibnux <me@ibnux.com>
- *
+ * Payment Gateway bkash.com
  **/
 
-
-function paypal_validate_config()
+function bkash_validate_config()
 {
     global $config;
-    if (empty($config['paypal_client_id']) || empty($config['paypal_secret_key'])) {
-        sendTelegram("PayPal payment gateway not configured");
-        r2(U . 'order/package', 'w', Lang::T("Admin has not yet setup Paypal payment gateway, please tell admin"));
+    if (empty($config['bkash_app_key']) || empty($config['bkash_app_secret']) || empty($config['bkash_username']) || empty($config['bkash_password'])) {
+        sendTelegram("BKASH payment gateway not configured");
+        r2(U . 'order/package', 'w', Lang::T("Admin has not yet setup BKASH payment gateway, please tell admin"));
     }
 }
 
-function paypal_show_config()
+
+function bkash_show_config()
 {
-    global $ui;
-    $ui->assign('_title', 'Paypal - Payment Gateway');
-    $ui->assign('currency', json_decode(file_get_contents('system/paymentgateway/paypal_currency.json'), true));
-    $ui->display('paypal.tpl');
+    global $ui, $config;
+    $ui->assign('_title', 'bKash - Tokenizer - Payment Gateway');
+    //$ui->assign('channels', json_decode(file_get_contents('system/paymentgateway/channel_duitku.json'), true));
+    $ui->display('bkash.tpl');
 }
 
 
-function paypal_save_config()
+function bkash_save_config()
 {
-    global $admin, $_L;
-    $paypal_client_id = _post('paypal_client_id');
-    $paypal_secret_key = _post('paypal_secret_key');
-    $paypal_currency = _post('paypal_currency');
-    $d = ORM::for_table('tbl_appconfig')->where('setting', 'paypal_secret_key')->find_one();
+    global $admin;
+    $bkash_app_key = _post('bkash_app_key');
+    $bkash_app_secret = _post('bkash_app_secret');
+    $bkash_username = _post('bkash_username');
+    $bkash_password = _post('bkash_password');
+    $d = ORM::for_table('tbl_appconfig')->where('setting', 'bkash_app_key')->find_one();
     if ($d) {
-        $d->value = $paypal_secret_key;
+        $d->value = $bkash_app_key;
         $d->save();
     } else {
         $d = ORM::for_table('tbl_appconfig')->create();
-        $d->setting = 'paypal_secret_key';
-        $d->value = $paypal_secret_key;
+        $d->setting = 'bkash_app_key';
+        $d->value = $bkash_app_key;
         $d->save();
     }
-    $d = ORM::for_table('tbl_appconfig')->where('setting', 'paypal_client_id')->find_one();
+    $d = ORM::for_table('tbl_appconfig')->where('setting', 'bkash_app_secret')->find_one();
     if ($d) {
-        $d->value = $paypal_client_id;
+        $d->value = $bkash_app_secret;
         $d->save();
     } else {
         $d = ORM::for_table('tbl_appconfig')->create();
-        $d->setting = 'paypal_client_id';
-        $d->value = $paypal_client_id;
+        $d->setting = 'bkash_app_secret';
+        $d->value = $bkash_app_secret;
         $d->save();
     }
-    $d = ORM::for_table('tbl_appconfig')->where('setting', 'paypal_currency')->find_one();
+    $d = ORM::for_table('tbl_appconfig')->where('setting', 'bkash_username')->find_one();
     if ($d) {
-        $d->value = $paypal_currency;
+        $d->value = $bkash_username;
         $d->save();
     } else {
         $d = ORM::for_table('tbl_appconfig')->create();
-        $d->setting = 'paypal_currency';
-        $d->value = $paypal_currency;
+        $d->setting = 'bkash_username';
+        $d->value = $bkash_username;
         $d->save();
     }
-    _log('[' . $admin['username'] . ']: Paypal ' . $_L['Settings_Saved_Successfully'], 'Admin', $admin['id']);
-
-    r2(U . 'paymentgateway/paypal', 's', $_L['Settings_Saved_Successfully']);
+    $d = ORM::for_table('tbl_appconfig')->where('setting', 'bkash_password')->find_one();
+    if ($d) {
+        $d->value = $bkash_password;
+        $d->save();
+    } else {
+        $d = ORM::for_table('tbl_appconfig')->create();
+        $d->setting = 'bkash_password';
+        $d->value = $bkash_password;
+        $d->save();
+    }
+    _log('[' . $admin['username'] . ']: bKash ' . Lang::T('Settings_Saved_Successfully'), 'Admin', $admin['id']);
+    r2(U . 'paymentgateway/bKash', 's', Lang::T('Settings_Saved_Successfully'));
 }
 
-function paypal_create_transaction($trx, $user)
+function bkash_create_transaction($trx, $user)
 {
     global $config;
     $json = [
-        'intent' => 'CAPTURE',
-        'purchase_units' => [
-            [
-                'amount' => [
-                    'currency_code' => $config['paypal_currency'],
-                    'value' => strval($trx['price'])
-                ]
-            ]
-        ],
-        "application_context" => [
-            "return_url" => U . "order/view/" . $trx['id'] . '/check',
-            "cancel_url" => U . "order/view/" . $trx['id'],
-        ]
+        'intent' => 'sale',
+        'mode' => '0011',
+        'payerReference' => $trx['id'],
+        'currency' => 'BDT',
+        'amount' => $trx['price'],
+        'callbackURL' => U . 'order/view/'.$trx['id'].'/check',
+        'merchantInvoiceNumber' => $trx['id'],
     ];
-    //die(json_encode($json,JSON_PRETTY_PRINT));
-
-    $result = json_decode(
-        Http::postJsonData(
-            paypal_get_server() . 'checkout/orders',
-            $json,
-            [
-                'Prefer: return=minimal',
-                'PayPal-Request-Id: paypal_' . $trx['id'],
-                'Authorization: Bearer ' . paypalGetAccessToken()
-            ]
-        ),
-        true
-    );
-    if (!$result['id']) {
-        sendTelegram("paypal_create_transaction FAILED: \n\n" . json_encode($result, JSON_PRETTY_PRINT));
-        r2(U . 'order/package', 'e', Lang::T("Failed to create Paypal transaction."));
-    }
-    $urlPayment = "";
-    foreach ($result['links'] as $link) {
-        if ($link['rel'] == 'approve') {
-            $urlPayment = $link['href'];
-            break;
-        }
+    $headers = ['Authorization: ' . bkash_get_token(), 'X-App-Key: ' . $config['bkash_app_key']];
+    $result = json_decode(Http::postJsonData(bkash_get_server() . 'checkout/create', $json, $headers), true);
+    if ($result['statusMessage'] != 'Successful') {
+        sendTelegram("bKash payment failed\n\n" . json_encode($result, JSON_PRETTY_PRINT));
+        r2(U . 'order/package', 'e', Lang::T("Failed to create transaction. ".$result['errorMessage']));
     }
     $d = ORM::for_table('tbl_payment_gateway')
         ->where('username', $user['username'])
         ->where('status', 1)
         ->find_one();
-    $d->gateway_trx_id = $result['id'];
-    $d->pg_url_payment = $urlPayment;
+    $d->gateway_trx_id = $result['paymentID'];
+    $d->pg_url_payment = $result['bkashURL'];
     $d->pg_request = json_encode($result);
-    $d->expired_date = date('Y-m-d H:i:s', strtotime("+ 6 HOUR"));
+    $d->expired_date = date('Y-m-d H:i:s', strtotime('+ 4 HOURS'));
     $d->save();
-    header('Location: ' . $urlPayment);
+    header('Location: ' . $result['bkashURL']);
     exit();
 }
 
-/*
-*/
-
-function paypal_payment_notification()
+function bkash_get_status($trx, $user)
 {
-    // Not yet implemented
-}
-
-function paypal_get_status($trx, $user)
-{
-    $result = json_decode(Http::getData(paypal_get_server() . 'checkout/orders/' . $trx['gateway_trx_id'], ['Authorization: Bearer ' . paypalGetAccessToken()]), true);
-    if (in_array($result['status'], ['APPROVED', 'COMPLETED']) && $trx['status'] != 2) {
-        if (!Package::rechargeUser($user['id'], $trx['routers'], $trx['plan_id'], $trx['gateway'], 'Paypal')) {
+    global $config;
+    Http::postJsonData(bkash_get_server() . 'checkout/execute', ['paymentID' => $trx['gateway_trx_id']], ['Authorization: ' . bkash_get_token(), 'X-App-Key: ' . $config['bkash_app_key']]);
+    $result = json_decode(Http::postJsonData(bkash_get_server() . 'checkout/payment/status', ['paymentID' => $trx['gateway_trx_id']], ['Authorization: ' . bkash_get_token(), 'X-App-Key: ' . $config['bkash_app_key']]), true);
+    if ($result['statusCode'] != '0000') {
+        sendTelegram("bKash payment status failed\n\n" . json_encode($result, JSON_PRETTY_PRINT));
+        r2(U . "order/view/" . $trx['id'], 'e', Lang::T("Failed to check status transaction. " . $result['errorMessage']));
+    }
+    if ($trx['status'] == 2) {
+        r2(U . "order/view/" . $trx['id'], 'd', Lang::T("Transaction has been paid.."));
+    }
+    if ($result['transactionStatus'] == 'Completed') {
+        if (!Package::rechargeUser($user['id'], $trx['routers'], $trx['plan_id'], $trx['gateway'],  'bKash')) {
             r2(U . "order/view/" . $trx['id'], 'd', Lang::T("Failed to activate your Package, try again later."));
         }
         $trx->pg_paid_response = json_encode($result);
-        $trx->payment_method = 'PAYPAL';
-        $trx->payment_channel = 'paypal';
-        $trx->paid_date = date('Y-m-d H:i:s', strtotime($result['updated']));
+        $trx->payment_method = 'bKash';
+        $trx->payment_channel = 'bKash';
+        $trx->paid_date = date('Y-m-d H:i:s', $result['paid_at']);
         $trx->status = 2;
         $trx->save();
+
         r2(U . "order/view/" . $trx['id'], 's', Lang::T("Transaction has been paid."));
-    } else if ($result['status'] == 'VOIDED') {
-        $trx->pg_paid_response = json_encode($result);
-        $trx->status = 3;
-        $trx->save();
-        r2(U . "order/view/" . $trx['id'], 'd', Lang::T("Transaction expired."));
     } else {
-        sendTelegram("xendit_get_status: unknown result\n\n" . json_encode($result, JSON_PRETTY_PRINT));
-        r2(U . "order/view/" . $trx['id'], 'w', "Transaction status :" . $result['status']);
+        print_r($result);
+        die();
+        r2(U . "order/view/" . $trx['id'], 'w', Lang::T("Transaction still unpaid."));
     }
 }
 
-function paypalGetAccessToken()
+// Callback
+function bkash_payment_notification()
 {
     global $config;
-    $result = Http::postData(str_replace('v2', 'v1', paypal_get_server()) . 'oauth2/token', [
-        "grant_type" => "client_credentials"
-    ], [], $config['paypal_client_id'] . ":" . $config['paypal_secret_key']);
-    $json = json_decode($result, true);
-    return $json['access_token'];
+    if ($_GET['status'] == 'success') {
+        $paymentID = $_GET['paymentID'];
+        //execute
+        json_decode(Http::postJsonData(bkash_get_server() . 'checkout/execute', ['paymentID' => $paymentID], ['Authorization: ' . bkash_get_token(), 'X-App-Key: ' . $config['bkash_app_key']]), true);
+        //query
+        # Just let the user check the payment from user page
+    } else {
+        sendTelegram("BKASH payment " . $_GET['status'] . " for paymentID: " . $_GET['paymentID']);
+    }
+}
+
+function bkash_get_token()
+{
+    global $config;
+    $json = [
+        'app_key' => $config['bkash_app_key'],
+        'app_secret' => $config['bkash_app_secret']
+    ];
+    $exp = $config['bkash_token_expired'];
+    $token = $config['bkash_token'];
+
+    if (time() < $exp) {
+        return $token;
+    }
+    if (!empty($config['bkash_refresh_token'])) {
+        $json['refresh_token'] = $config['bkash_refresh'];
+        $url = bkash_get_server() . 'checkout/token/refresh';
+    } else {
+        $url = bkash_get_server() . 'checkout/token/grant';
+    }
+    $headers = ['username: '.$config['bkash_username'], 'password: '. $config['bkash_password']];
+    $result = json_decode(Http::postJsonData($url, $json, $headers), true);
+    if ($result['statusMessage'] == 'Successful') {
+        // if has refresh token from server, save it
+        if (!empty($result['refresh_token'])) {
+            $d = ORM::for_table('tbl_appconfig')->where('setting', 'bkash_refresh_token')->find_one();
+            if ($d) {
+                $d->value = $result['refresh_token'];
+                $d->save();
+            } else {
+                $d = ORM::for_table('tbl_appconfig')->create();
+                $d->setting = 'bkash_refresh_token';
+                $d->value = $result['refresh_token'];
+                $d->save();
+            }
+        }
+        // save token
+        $d = ORM::for_table('tbl_appconfig')->where('setting', 'bkash_token')->find_one();
+        if ($d) {
+            $d->value = $result['id_token'];
+            $d->save();
+        } else {
+            $d = ORM::for_table('tbl_appconfig')->create();
+            $d->setting = 'bkash_token';
+            $d->value = $result['id_token'];
+            $d->save();
+        }
+        // save expire
+        $d = ORM::for_table('tbl_appconfig')->where('setting', 'bkash_token_expired')->find_one();
+        if ($d) {
+            $d->value = time() + ($result['expires_in'] - 10);
+            $d->save();
+        } else {
+            $d = ORM::for_table('tbl_appconfig')->create();
+            $d->setting = 'bkash_token_expired';
+            $d->value = time() + ($result['expires_in'] - 10);
+            $d->save();
+        }
+        return $result['id_token'];
+    }else{
+        sendTelegram("bKash payment failed\n\n" . json_encode($result, JSON_PRETTY_PRINT));
+        r2(U . 'order/package', 'e', Lang::T("Failed to create transaction. ".$result['errorMessage']));
+    }
 }
 
 
-function paypal_get_server()
+function bkash_get_server()
 {
     global $_app_stage;
     if ($_app_stage == 'Live') {
-        return 'https://api-m.paypal.com/v2/';
+        return 'https://tokenized.bka.sh/v1.2.0-beta/tokenized/';
     } else {
-        /**
-         * Wallet Number: 01877722345
-         * OTP: 123456
-         * Pin: 12121
-         */
-        return 'https://api-m.sandbox.paypal.com/v2/';
+        return 'https://tokenized.sandbox.bka.sh/v1.2.0-beta/tokenized/';
     }
 }
